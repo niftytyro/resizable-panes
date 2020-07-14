f63a1dcc279e4f1700eb355a0d6f222d9fbb3ddf
udasitharani <udasitharani@gmail.com>
2020-07-15 00:43:50 +0530

    complete code

--- index.js
+++ index.js
@@ -44,6 +44,9 @@ function initialize() {
     BOTTOM_HEIGHT = bottom_pane.height();
     DISPLAY_WIDTH = $(document).width();
     DISPLAY_HEIGHT = $(document).height();
+    display_pane.css('max-width', ($(document).width() - RIGHT_WIDTH - LEFT_WIDTH - 6) + 'px');
+    top_view.css('max-height', ($(document).height() - 213) + 'px');
+    top_view.height($(document).height() - bottom_pane.height() - 3);
 
 
     top_view.css('height', (DISPLAY_HEIGHT - BOTTOM_HEIGHT - 3) + 'px');
@@ -53,31 +56,67 @@ function initialize() {
         initX = event.pageX;
         initY = event.pageY;
         dragging = true;
-        resize_element = left_pane;
+        resize_element = 'left';
     });
     right_pane_border.mousedown(function (event) {
         initX = event.pageX;
         initY = event.pageY;
         dragging = true;
-        resize_element = right_pane;
+        resize_element = 'right';
+        console.log('right')
     });
     bottom_pane_border.mousedown(function (event) {
         initX = event.pageX;
         initY = event.pageY;
         dragging = true;
-        resize_element = bottom_pane;
+        resize_element = 'bottom';
     });
     $(document).mouseup(function (event) {
         dragging = false;
     });
     $(document).mousemove(function (event) {
         if (dragging) {
-            differenceX = event.pageX - initX;
-            differenceY = event.pageY - initY;
-            left_pane.css('width', left_pane.width() + differenceX + 'px');
-            initX = event.pageX;
-            initY = event.pageY;
-            console.log(left_pane.width());
+            if (resize_element == 'left') {
+                differenceX = event.pageX - initX;
+                var max = left_pane.css('max-width').substr(0, left_pane.css('max-width').indexOf('px'));
+                if (((max > left_pane.width()) && (differenceX > 0)) || (differenceX < 0)) {
+                    console.log(differenceX);
+                    left_pane.css('width', left_pane.width() + differenceX / 2 + 'px');
+                    if (right_pane.css('min-width') <= right_pane.width()) {
+                        right_pane.css('width', right_pane.width() - differenceX / 4 + 'px');
+                        display_pane.css('width', display_pane.width() - differenceX / 4 + 'px');
+                    } else {
+                        display_pane.css('width', display_pane.width() - differenceX / 2 + 'px');
+                    }
+                    initX = event.pageX;
+                    initY = event.pageY;
+                }
+            }
+            else if (resize_element == 'bottom') {
+                differenceY = initY - event.pageY;
+                var max = bottom_pane.css('max-height').substr(0, bottom_pane.css('max-height').indexOf('px'));
+                if (((max > bottom_pane.height()) && (differenceY > 0)) || (differenceY < 0)) {
+                    bottom_pane.css('height', bottom_pane.height() + differenceY / 2 + 'px');
+                    top_view.css('height', top_view.height() - differenceY / 2 + 'px');
+                    initX = event.pageX;
+                    initY = event.pageY;
+                }
+            }
+            else if (resize_element == 'right') {
+                differenceX = initX - event.pageX;
+                var max = right_pane.css('max-width').substr(0, right_pane.css('max-width').indexOf('px'));
+                if (((max > right_pane.width()) && (differenceX > 0)) || (differenceX < 0)) {
+                    right_pane.css('width', right_pane.width() + differenceX / 2 + 'px');
+                    if (left_pane.css('min-width') <= left_pane.width()) {
+                        left_pane.css('width', left_pane.width() - differenceX / 4 + 'px');
+                        display_pane.css('width', display_pane.width() - differenceX / 4 + 'px');
+                    } else {
+                        display_pane.css('width', display_pane.width() - differenceX / 2 + 'px');
+                    }
+                    initX = event.pageX;
+                    initY = event.pageY;
+                }
+            }
         }
     });
 }
